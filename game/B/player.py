2837092975049346251578762874fa6054eea28e
TeddyTjoe1990 <her21059@byui.edu>
2022-02-25 16:53:58 +0700

    player class has made

---
+++ game/B/player.py
@@ -0,0 +1,73 @@
+class player:
+    """A person who directs the game. 
+
+    The responsibility of a player is to control the sequence of play.
+
+    Attributes:
+        display : For providing video output.
+        keyboard : For getting directional input.
+    """
+
+    def __init__(self, keyboard, display):
+        """Constructs a new Director using the specified keyboard and video services.
+
+        Args:
+            display : An instance of game display.
+            keyboard : An instance of keyboard console.   
+        """
+        self.keyboard = keyboard
+        self.display = display
+
+    def start_game(self, cast):
+        """Starts the game using the given cast. Runs the main game loop.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self._get_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
+        self._video_service.close_window()
+
+    def _get_inputs(self, cast):
+        """Gets directional input from the keyboard and applies it to the robot.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        robot = cast.get_first_actor("robots")
+        velocity = self._keyboard_service.get_direction()
+        robot.set_velocity(velocity)
+
+    def _do_updates(self, cast):
+        """Updates the robot's position and resolves any collisions with artifacts.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        banner = cast.get_first_actor("banners")
+        robot = cast.get_first_actor("robots")
+        artifacts = cast.get_actors("artifacts")
+
+        banner.set_text("")
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        robot.move_next(max_x, max_y)
+
+        for artifact in artifacts:
+            if robot.get_position().equals(artifact.get_position()):
+                message = artifact.get_message()
+                banner.set_text(message)
+
+    def _do_outputs(self, cast):
+        """Draws the actors on the screen.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.clear_buffer()
+        actors = cast.get_all_actors()
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
